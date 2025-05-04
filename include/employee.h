34ca053d521ff768f8af6f86db6eae802bcf4545
HP-VICTUS 16 <v.yahnavets@gmail.com>
2025-05-04 20:04:09 +0300

    created employee.h

---
+++ include/employee.h
@@ -0,0 +1,14 @@
+//
+// employee.h
+//
+
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+struct employee {
+    int num;
+    char name[10];
+    double hours;
+};
+
+#endif //EMPLOYEE_H
---
+++ src/creator.cpp
@@ -0,0 +1,3 @@
+//
+// Created by HP-VICTUS 16 on 04.05.2025.
+//
