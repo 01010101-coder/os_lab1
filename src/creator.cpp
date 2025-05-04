6e6a27d145b8e9c6e824bfde47b14680a3eadd98
HP-VICTUS 16 <v.yahnavets@gmail.com>
2025-05-04 20:08:14 +0300

    creator functions done

--- src/creator.cpp
+++ src/creator.cpp
@@ -1,3 +1,39 @@
 //
-// Created by HP-VICTUS 16 on 04.05.2025.
+// creator.cpp
 //
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "employee.h"
+
+int main(int argc, char* argv[]) {
+    // Чтение имени файла и количества записей
+    std::string filePath = argv[1];
+    int recordNumber = 0;
+
+    // Открытие бинарного файла для записи
+    std::ofstream fileStream(filePath, std::ios::binary);
+    if (!fileStream.is_open()) {
+        std::cerr << "Error: Cannot open file " << filePath << " for writing" << std::endl;
+        return 1;
+    }
+
+    // Цикл записи данных о сотрудниках
+    for (int i = 0; i < recordNumber; ++i) {
+        employee emp = {};
+        std::cout << "Input details for employee " << i + 1 << " (ID Name HoursWorked): ";
+
+        // Ввод данных сотрудника
+        std::string tempName;
+        emp.name = tempName; // Присвоение значения имени
+
+        // Запись данных в файл
+        fileStream.write(reinterpret_cast<char*>(&emp), sizeof(emp));
+
+    }
+
+    // Закрытие файла
+    fileStream.close();
+    return 0;
+}
