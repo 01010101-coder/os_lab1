9545c4a9aeeb972460af554c57d3aeadf1d51487
HP-VICTUS 16 <v.yahnavets@gmail.com>
2025-05-04 20:36:41 +0300

    main functions done

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,66 @@
+#include <windows.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "../include/employee.h"
+
+// Функция для вывода содержимого бинарного файла в консоль
+void displayBinaryData(const std::string& filename) {
+    std::ifstream inputFile(filename, std::ios::binary);
+
+    employee emp;
+    while (inputFile.read(reinterpret_cast<char*>(&emp), sizeof(emp))) {
+        std::cout << "Employee Number: " << emp.num << " Name: " << emp.name
+                  << " Worked Hours: " << emp.hours << std::endl;
+    }
+}
+
+// Функция для вывода содержимого отчетного файла в консоль
+void showReportContent(const std::string& filename) {
+    std::ifstream reportFile(filename);
+
+    std::string line;
+    while (std::getline(reportFile, line)) {
+        std::cout << line << std::endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string binaryFileName;
+    int totalRecords = 0;
+
+    std::cout << "Enter binary file name: ";
+    std::cin >> binaryFileName;
+    std::cout << "Enter number of records: ";
+    std::cin >> totalRecords;
+
+    // Формируем команду для запуска процесса Creator.exe
+    std::string creatorCommand = "Creator.exe " + binaryFileName + " " + std::to_string(totalRecords);
+    STARTUPINFO startupInfo = { sizeof(startupInfo) };
+    PROCESS_INFORMATION processInfo;
+
+    // Ожидаем завершения процесса Creator
+    WaitForSingleObject(processInfo.hProcess, INFINITE);
+    CloseHandle(processInfo.hProcess);
+    CloseHandle(processInfo.hThread);
+
+    displayBinaryData(binaryFileName);
+
+    std::string reportFile;
+    double ratePerHour = 0;
+
+    std::cout << "Enter report file name: ";
+    std::cin >> reportFile;
+    std::cout << "Enter hourly rate: ";
+    std::cin >> ratePerHour;
+
+    std::string reporterCommand = "Reporter.exe " + binaryFileName + " " + reportFile + " " + std::to_string(ratePerHour);
+
+    // Ожидаем завершения процесса Reporter
+    WaitForSingleObject(processInfo.hProcess, INFINITE);
+    CloseHandle(processInfo.hProcess);
+    CloseHandle(processInfo.hThread);
+
+    showReportContent(reportFile);
     return 0;
 }
